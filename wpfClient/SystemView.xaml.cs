2afd7fcf50d4051ece124cb55fe58f215d2d4ce9
jafowler <jacobtfowler@gmail.com>
2016-04-13 20:41:53 -0400

    working gui
    working gui with localhost

--- wpfClient/SystemView.xaml.cs
+++ wpfClient/SystemView.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,17 +12,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Runtime.Serialization;
-using System.IO;
+using sharedObjects;
 
 namespace wpfClient
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SystemView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SystemView : UserControl
     {
-        public MainWindow()
+        public SystemView()
         {
             InitializeComponent();
         }
---
+++ wpfClient/SystemViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sharedObjects;
+using System.Net;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace wpfClient
+{
+    class SystemViewModel : INotifyPropertyChanged
+    {
+        public SystemViewModel()
+        {
+            //string ipAddress = "http://192.168.0.";
+            NetworkSystems = new ObservableCollection<SysInfo>();
+            var retval = getActiveSystemInformation("http://localhost");
+            if (retval != null) NetworkSystems.Add(retval);
+        }
+
+       public event PropertyChangedEventHandler PropertyChanged;
+
+        public SysInfo getActiveSystemInformation(string ipAddress)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(ipAddress + ":8089/ServerApp/devices");
+            Trace.WriteLine(ipAddress + ":8089");
+            request.Method = "GET";
+
+            try
+            {
+                var response = request.GetResponse();
+                var stream = response.GetResponseStream();
+                var dataSerializer = new DataContractSerializer(typeof(SysInfo));
+                var test = (SysInfo)dataSerializer.ReadObject(stream);
+                return test;
+            }
+            catch (WebException we)
+            {
+                Trace.WriteLine(we);
+                return null;
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e);
+                return null;
+            }
+        }
+
+        public ObservableCollection<SysInfo> NetworkSystems
+        {
+            get; private set;
+        }
+    }
+        
+}
+
+    
+
