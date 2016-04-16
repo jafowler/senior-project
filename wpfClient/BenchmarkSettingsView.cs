d815017ec49987bf6fe1c7d49024077772cc2b8f
jafowler <jacobtfowler@gmail.com>
2016-04-16 12:16:34 -0400

    adding benchmarksettings.cs

---
+++ wpfClient/BenchmarkSettingsView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sharedObjects;
+using wpfClient;
+
+namespace wpfClient
+{
+    class BenchmarkSettingsModel
+    {
+        public BenchmarkSettingsModel()
+        {   
+
+        }
+
+        public BenchmarkSettings benchmarkSettings { get; set; }
+    }
+    
+}
